b0404a6b87c0e291ca8827ace4a8296096fcb015
Αργυροκαστρίτης Ιωάννης - Σπυρίδων <94742350+Argyrokastritis@users.noreply.github.com>
2023-10-31 12:45:38 +0200

    Dcumentation added to HomeWindow.py

--- LoRaSim/gui/HomeWindow.py
+++ LoRaSim/gui/HomeWindow.py
@@ -12,7 +12,19 @@ from LoRaSim.gui.Plotter import Plotter
 
 
 class HomeWindow(QtWidgets.QWidget):
+    """
+        A class representing the main window of the LoRaSim application.
+
+        Attributes:
+            simulator (Simulator): An instance of the Simulator class.
+            simIntView (QListWidget): A QListWidget that displays simulation intervals.
+            plotOptPanel (PlotOptPanel): A PlotOptPanel that allows the user to select which plots to display.
+            start_btn (QPushButton): A QPushButton that starts the simulation.
+        """
     def __init__(self):
+        """
+            Initializes the HomeWindow class and creates an instance of the Simulator class.
+        """
         super(QtWidgets.QWidget, self).__init__()
         self.simulator = Simulator()
 
@@ -23,6 +35,10 @@ class HomeWindow(QtWidgets.QWidget):
         self.initUI()
 
     def initUI(self):
+        """
+            Sets up the user interface by creating a vertical layout and adding a SimIntervalsView and a PlotOptPanel.
+            Also creates a start button with a media-playback-start icon.
+        """
         self.setLayout(QtWidgets.QVBoxLayout())
         self.simIntView = self.createSimIntView()
         self.plotOptPanel = self.createPlotOptPanel()
@@ -32,16 +48,34 @@ class HomeWindow(QtWidgets.QWidget):
 
 
     def createSimIntView(self):
+        """
+            Creates a SimIntervalsView object and adds it to the layout.
+
+            Returns:
+            SimIntervalsView: A SimIntervalsView object.
+        """
         s = SimIntervalsView(self.simulator)
         self.layout().addWidget(s)
         return s
 
     def createPlotOptPanel(self):
+        """
+        Creates a PlotOptPanel object and adds it to the layout.
+
+            Returns:
+            PlotOptPanel: A PlotOptPanel object.
+        """
         p = PlotOptPanel()
         self.layout().addWidget(p)
         return p
 
     def createStartBtn(self):
+        """
+        Creates a QPushButton object with a media-playback-start icon and connects it to the runSimulation method.
+
+            Returns:
+            QPushButton: A QPushButton object.
+        """
         b = QtWidgets.QPushButton()
         b.setText("Start Simulation")
         b.setIcon(QtGui.QIcon.fromTheme('media-playback-start'))
@@ -51,13 +85,24 @@ class HomeWindow(QtWidgets.QWidget):
         return b
 
     def runSimulation(self):
+        """
+                Runs the simulation and creates a Plotter object. If the getRcvProbOption method of the plotOptPanel object returns true,
+                 it plots the reception probability. If the getThroughputOption method of the plotOptPanel object returns true,
+                 it plots the throughput. Finally, it shows the plots using the show_plots method of the Plotter object.
+        """
+        # Runs simulation
         data = self.simulator.run()
 
+        # Creates plotter
         p = Plotter(data)
-        
+
+        # Plots receive probability if selected
         if self.plotOptPanel.getRcvProbOption():
             p.plot_rcv_prob()
+
+        # Plots throughput if selected
         if self.plotOptPanel.getThroughputOption():
             p.plot_throughput()
 
+        # Shows plots
         p.show_plots()
